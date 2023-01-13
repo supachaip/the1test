28c0cfed43166169f66e15f061c35b1f6e2f4cd9
Supachai Pornpisanukitcha <supachai.p71@gmail.com>
2023-01-13 16:24:34 +0700

    init spring boot

---
+++ src/main/java/com/supachai/theonetest/TheoneTestApplication.java
@@ -0,0 +1,13 @@
+package com.supachai.theonetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheoneTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheoneTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supachai/theonetest/TheoneTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supachai.theonetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheoneTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
